789fe6001a66156ae13c95f49866f9e526a0e535
deividas-butkus <deividas.butkus@hotmail.com>
2024-10-25 12:29:34 +0300

    Create UserNavBox component and set initial structure.

---
+++ client/src/components/molecules/UsrNavBox.tsx
@@ -0,0 +1,13 @@
+const UserNavBox = () => {
+  return (
+    <div>
+      <div>Avatar</div>
+      <div>
+        <div>username</div>
+        <div>Logout</div>
+      </div>
+    </div>
+  );
+};
+
+export default UserNavBox;
