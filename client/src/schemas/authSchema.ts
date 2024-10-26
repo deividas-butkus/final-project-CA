d9a21dda85d51ff3bc109e5410169e21f97712fb
deividas-butkus <deividas.butkus@hotmail.com>
2024-10-26 19:04:03 +0300

    Create validation authSchema.

---
+++ client/src/schemas/authSchema.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const authSchema = z
+  .object({
+    username: z
+      .string()
+      .min(5, "Username must be at least 5 characters")
+      .max(20, "Username must be no more than 20 characters"),
+    password: z
+      .string()
+      .min(5, "Password must be at least 5 characters")
+      .max(20, "Password must be no more than 20 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(
+        /[!@#$%^&*_+]/,
+        "Password must contain at least one special character (!@#$%^&*_+)"
+      ),
+    passwordRepeat: z.string().optional(),
+    profileImage: z.instanceof(File).optional(),
+  })
+  .refine(
+    (data) => data.password === data.passwordRepeat || !data.passwordRepeat,
+    {
+      message: "Passwords do not match",
+      path: ["passwordRepeat"],
+    }
+  );
