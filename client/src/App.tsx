a51ed3bc548769dd63f5323af5412d696bdfcce8
deividas-butkus <deividas.butkus@hotmail.com>
2024-10-29 19:08:30 +0200

    Set route for MyProfile.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Layout from "./components/templates/Layout";
 import Landing from "./components/organisms/mains/Landing";
 import Register from "./components/organisms/mains/Register";
 import Login from "./components/organisms/mains/Login";
+import MyProfile from "./components/organisms/mains/MyProfile";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/user" element={<MyProfile />} />
         </Route>
       </Routes>
     </>
