69e63b7155318c95298ca111bf605555df78c58f
deividas-butkus <deividas.butkus@hotmail.com>
2024-10-29 22:17:12 +0200

    Apply logout function in UserNavBox.

--- client/src/components/molecules/UserNavBox.tsx
+++ client/src/components/molecules/UserNavBox.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 import Avatar from "../atoms/Avatar";
 import { useUsersContext } from "../../contexts/users/useUsersContext";
@@ -8,19 +10,42 @@ const StyledDiv = styled.div`
   justify-content: space-between;
   align-items: center;
   gap: 10px;
+  div.username {
+    color: ${({ theme }) => theme.accent};
+  }
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 7px;
+  background-color: transparent;
+  color: ${({ theme }) => theme.text};
+  padding: 0;
+  border: none;
+  outline: none;
+  box-shadow: none;
+  cursor: pointer;
 `;
 
 const UserNavBox = () => {
-  const { currentUser } = useUsersContext();
+  const { currentUser, logout } = useUsersContext();
 
   return (
     <StyledDiv>
       <div>
-        <Avatar />
+        <NavLink to="/user">
+          <Avatar />
+        </NavLink>
       </div>
       <div>
-        <div>{currentUser?.username}</div>
-        <div>Logout</div>
+        <NavLink to="/user">
+          <div className="username">{currentUser?.username}</div>
+        </NavLink>
+        <StyledButton onClick={logout}>
+          <span>Logout</span>
+          <LogoutIcon />
+        </StyledButton>
       </div>
     </StyledDiv>
   );
