56e77c6f13d68b0d5738231cb63babd7403e229e
deividas-butkus <deividas.butkus@hotmail.com>
2024-10-30 19:17:27 +0200

    Apply chatsRouter in server.

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 
 import { connectToDb } from "./mongoClient.js";
 import usersRouter from "./routes/users.js";
+import chatsRouter from "./routes/chats.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -30,6 +31,8 @@ app.use(express.json());
 app.use("/users", usersRouter);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use("/chats", chatsRouter);
 
 const startServer = async () => {
   try {
