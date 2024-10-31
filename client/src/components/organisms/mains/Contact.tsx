1fc0d87ba1c0f045ea2b221b927bf19168b3acb7
deividas-butkus <deividas.butkus@hotmail.com>
2024-10-31 12:44:56 +0200

    Create Contact.

---
+++ client/src/components/organisms/mains/Contact.tsx
@@ -0,0 +1,16 @@
+import Button from "../../atoms/Button";
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact X</h2>
+      <h3>username X</h3>
+      <div className="profileImage">
+        <img src="#" alt="User's X image" />
+      </div>
+      <Button>Chat with contact X</Button>
+    </section>
+  );
+};
+
+export default Contact;
