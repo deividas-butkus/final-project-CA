4b79499ed0c98bde95a277b56eb190ef1099396f
deividas-butkus <deividas.butkus@hotmail.com>
2024-11-02 15:31:05 +0200

    Fetch and style messages in chat.

--- client/src/components/molecules/MessageCard.tsx
+++ client/src/components/molecules/MessageCard.tsx
@@ -1,32 +1,37 @@
 import styled from "styled-components";
+import { Message } from "../../types/MessagesTypes";
 
-const StyledArticle = styled.article`
+const StyledArticle = styled.article<{ $isCurrentUser: boolean }>`
   width: 200px;
   padding: 10px;
   border-radius: 10px;
+  background-color: ${(props) =>
+    props.$isCurrentUser ? "#daf5e3" : "#c44228"};
+  align-self: ${(props) => (props.$isCurrentUser ? "flex-end" : "flex-start")};
+  color: black;
+  text-align: ${(props) => (props.$isCurrentUser ? "right" : "left")};
   > div {
     display: flex;
     flex-direction: column;
-    align-items: center;
-  }
-  > svg {
-    display: flex;
-    justify-self: flex-end;
+    align-items: flex-start;
   }
 `;
 
 export type MessageCardProps = {
   message: {
-    content: string;
-    isRead: boolean;
-    createdAt: string;
+    _id: Message["_id"];
+    content: Message["content"];
+    isRead: Message["isRead"];
+    createdAt: Message["createdAt"];
   };
+  isCurrentUser: boolean;
 };
 
-const MessageCard = ({ message }: MessageCardProps) => {
+const MessageCard = ({ message, isCurrentUser }: MessageCardProps) => {
   return (
-    <StyledArticle>
+    <StyledArticle $isCurrentUser={isCurrentUser}>
       <div>
+        <p>Message ID: {message._id}</p>
         <p>{message.content}</p>
         <p>{message.isRead ? "Read" : "Unread"}</p>
         <p>{new Date(message.createdAt).toLocaleString()}</p>
