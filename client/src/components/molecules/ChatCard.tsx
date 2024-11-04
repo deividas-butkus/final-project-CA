7ecd1856a6b931bdc585d09979b0e21a0c965416
deividas-butkus <deividas.butkus@hotmail.com>
2024-11-04 13:15:49 +0200

    Implement deleteChat in ChatCard and style ChatCard and Chats components.

--- client/src/components/molecules/ChatCard.tsx
+++ client/src/components/molecules/ChatCard.tsx
@@ -2,28 +2,51 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useUsersContext } from "../../contexts/users/useUsersContext";
 import { Chat as ChatType } from "../../types/ChatsTypes";
+import Button from "../atoms/Button";
+import { useChatsContext } from "../../contexts/chats/useChatsContext";
 
 const StyledDiv = styled.div`
   height: auto;
-  > h4 {
-    > a {
-      color: ${({ theme }) => theme.accent};
-      text-decoration: none;
-      &:hover {
-        text-decoration: underline;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  > a {
+    > div {
+      display: flex;
+      gap: 30px;
+      min-width: 500px;
+      border-radius: 7px;
+      padding: 5px 10px;
+      box-shadow: 1px 1px 3px ${({ theme }) => theme.accent};
+
+      > img {
+        height: 70px;
+        width: 70px;
+        object-fit: cover;
+        border-radius: 7px;
+      }
+      > div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 10px;
+        h4 {
+          margin: 0;
+          color: ${({ theme }) => theme.accent};
+        }
+        p {
+          margin: 0;
+          color: #fff;
+        }
+        > p.timestamp {
+          font-size: small;
+          font-weight: 100;
+        }
       }
     }
   }
-  > div {
-    display: flex;
-    gap: 20px;
-    > img {
-      height: 100px;
-      width: 100px;
-      object-fit: cover;
-      border-radius: 7px;
-      box-shadow: 0.5px 0.5px 1px ${({ theme }) => theme.accent};
-    }
+  > button:hover {
+    background-color: ${({ theme }) => theme.error};
   }
 `;
 
@@ -33,6 +56,7 @@ type Props = {
 
 const ChatCard = ({ chat }: Props) => {
   const { currentUser } = useUsersContext();
+  const { deleteChat } = useChatsContext();
 
   const isSelfChat = chat.members.length === 1;
   const otherUser = chat.memberDetails?.find(
@@ -42,52 +66,57 @@ const ChatCard = ({ chat }: Props) => {
     ? "Store something for myself"
     : `Chat with ${otherUser?.username || "Unknown User"}`;
 
+  const handleDelete = () => {
+    deleteChat(chat._id);
+  };
+
   return (
     <StyledDiv>
-      <h4>
-        <Link to={`/chats/chat/${chat._id}`}>{chatTitle}</Link>
-      </h4>
-      <div>
-        {isSelfChat ? (
-          <img
-            src={
-              currentUser?.profileImage || "/uploads/defaultProfileImage.png"
-            }
-            alt={`${currentUser?.username}'s avatar`}
-          />
-        ) : (
-          otherUser?.profileImage && (
-            <img
-              src={otherUser.profileImage}
-              alt={`${otherUser.username}'s avatar`}
-            />
-          )
-        )}
+      <Link to={`/chats/chat/${chat._id}`}>
         <div>
-          {chat.lastMessage ? (
-            <>
-              <p>
-                {chat.lastMessage.content || "No message content available"}
-              </p>
-              <p>
-                {chat.lastMessage.createdAt
-                  ? new Date(chat.lastMessage.createdAt).toLocaleString(
-                      undefined,
-                      {
-                        month: "short",
-                        day: "numeric",
-                        hour: "numeric",
-                        minute: "numeric",
-                      }
-                    )
-                  : "No timestamp available"}
-              </p>
-            </>
+          {isSelfChat ? (
+            <img
+              src={
+                currentUser?.profileImage || "/uploads/defaultProfileImage.png"
+              }
+              alt={`${currentUser?.username}'s avatar`}
+            />
           ) : (
-            <p>No messages yet</p>
+            otherUser?.profileImage && (
+              <img
+                src={otherUser.profileImage}
+                alt={`${otherUser.username}'s avatar`}
+              />
+            )
           )}
+          <div>
+            <h4>{chatTitle}</h4>
+            {chat.lastMessage ? (
+              <>
+                <p>
+                  {chat.lastMessage.content || "No message content available"}
+                </p>
+                <p className="timestamp">
+                  {chat.lastMessage.createdAt
+                    ? new Date(chat.lastMessage.createdAt).toLocaleString(
+                        undefined,
+                        {
+                          month: "short",
+                          day: "numeric",
+                          hour: "numeric",
+                          minute: "numeric",
+                        }
+                      )
+                    : "No timestamp available"}
+                </p>
+              </>
+            ) : (
+              <p>No messages yet</p>
+            )}
+          </div>
         </div>
-      </div>
+      </Link>
+      <Button onClick={handleDelete}>Delete Chat</Button>
     </StyledDiv>
   );
 };
