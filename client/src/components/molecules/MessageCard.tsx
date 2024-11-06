80a944772ace89ce8a1af094f851d59b16345c7d
deividas-butkus <deividas.butkus@hotmail.com>
2024-11-06 09:31:52 +0200

    Implement like toggling functionality and styling in MessageCard.

--- client/src/components/molecules/MessageCard.tsx
+++ client/src/components/molecules/MessageCard.tsx
@@ -1,19 +1,38 @@
+import { useState } from "react";
 import styled from "styled-components";
+
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import { Message } from "../../types/MessagesTypes";
 
 const StyledArticle = styled.article<{ $isCurrentUser: boolean }>`
+  position: relative;
   width: 200px;
   padding: 10px;
   border-radius: 10px;
   background-color: ${(props) =>
-    props.$isCurrentUser ? "#daf5e3" : "#c44228"};
+    props.$isCurrentUser ? "#ac867e" : "#daf5e3"};
   align-self: ${(props) => (props.$isCurrentUser ? "flex-end" : "flex-start")};
   color: black;
   text-align: ${(props) => (props.$isCurrentUser ? "right" : "left")};
+
   > div {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+  }
+`;
+
+const StyledLikeIcon = styled(ThumbUpIcon)<{ $isLiked: boolean }>`
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: ${(props) => (props.$isLiked ? props.theme.active : "grey")};
+  cursor: pointer;
+  transition: color 0.3s;
+
+  &:hover {
+    color: ${(props) =>
+      props.$isLiked ? props.theme.activeHover : "lightgrey"};
   }
 `;
 
@@ -22,13 +41,33 @@ export type MessageCardProps = {
     _id: Message["_id"];
     content: Message["content"];
     createdAt: Message["createdAt"];
+    likedUserId?: string;
   };
   isCurrentUser: boolean;
+  onToggleLike: (messageId: string) => void;
+  $isLiked: boolean;
 };
 
-const MessageCard = ({ message, isCurrentUser }: MessageCardProps) => {
+const MessageCard = ({
+  message,
+  isCurrentUser,
+  onToggleLike,
+  $isLiked: initialIsLiked,
+}: MessageCardProps) => {
+  const [isLiked, setIsLiked] = useState(initialIsLiked);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleLikeClick = () => {
+    setIsLiked(!isLiked);
+    onToggleLike(message._id);
+  };
+
   return (
-    <StyledArticle $isCurrentUser={isCurrentUser}>
+    <StyledArticle
+      $isCurrentUser={isCurrentUser}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div>
         <p>{message.content}</p>
         <p>
@@ -39,6 +78,9 @@ const MessageCard = ({ message, isCurrentUser }: MessageCardProps) => {
             minute: "numeric",
           })}
         </p>
+        {!isCurrentUser && (isHovered || isLiked) && (
+          <StyledLikeIcon $isLiked={isLiked} onClick={handleLikeClick} />
+        )}
       </div>
     </StyledArticle>
   );
