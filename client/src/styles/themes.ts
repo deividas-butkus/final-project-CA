ad1c0588ba66141f0f846986021deb62f8a4e069
deividas-butkus <deividas.butkus@hotmail.com>
2024-11-07 19:44:36 +0200

    Adjust themes light mode background color and accent color, and transform color codes to lower case.

--- client/src/styles/themes.ts
+++ client/src/styles/themes.ts
@@ -1,11 +1,11 @@
 export const lightTheme = {
-  background: "#fff",
+  background: "#eee",
   text: "#2e2e2e",
-  accent: "#4E84C5",
+  accent: "#195470",
   buttonBg: "#8de5f2",
   buttonText: "#243b55",
-  border: "#1F3B4D",
-  error: "#D91924",
+  border: "#1f3b4d",
+  error: "#d91924",
   active: "#ff4500",
 };
 
@@ -15,7 +15,7 @@ export const darkTheme = {
   accent: "#60c1c7",
   buttonBg: "#243b55",
   buttonText: "#8de5f2",
-  border: "#1F3B4D",
+  border: "#1f3b4d",
   error: "#FE0605",
   active: "#ff4500",
 };
